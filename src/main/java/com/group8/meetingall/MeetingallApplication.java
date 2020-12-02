872eb74f46d836c9a85d91eacf841b7b45df9621
felicityamiao <1760077814@qq.com>
2020-12-03 07:54:20 +0800

    init project

---
+++ src/main/java/com/group8/meetingall/MeetingallApplication.java
@@ -0,0 +1,13 @@
+package com.group8.meetingall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group8/meetingall/MeetingallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group8.meetingall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
