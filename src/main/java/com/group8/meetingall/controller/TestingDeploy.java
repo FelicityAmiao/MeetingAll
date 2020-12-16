1613602c2dc19ce0267ae23013b9621195cc934e
CHENFE2 <felicity.chen@cargosmart.com>
2020-12-17 06:24:06 +0800

    [Felicity] Testing deploy in server.

---
+++ src/main/java/com/group8/meetingall/controller/TestingDeploy.java
@@ -0,0 +1,14 @@
+package com.group8.meetingall.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestingDeploy {
+
+  @RequestMapping("/hello")
+  public String greet() {
+    return "Hello guys!";
+  }
+}
