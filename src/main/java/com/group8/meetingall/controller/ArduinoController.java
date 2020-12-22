4cc3280797e5128cb24829afb0421fc6369c5909
AKIN GAN <akin.j.a.gan@oocl.com>
2020-12-22 19:17:07 +0800

    add the ArduinoController.

---
+++ src/main/java/com/group8/meetingall/controller/ArduinoController.java
@@ -0,0 +1,17 @@
+package com.group8.meetingall.controller;
+
+import com.group8.meetingall.dto.MeetingStatusDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/arduino")
+public class ArduinoController {
+
+  @PostMapping("/status")
+  public String receiveMeetingStatus(@RequestBody MeetingStatusDto meetingStatusDto) {
+    Optional<MeetingStatusDto> op = Optional.ofNullable(meetingStatusDto);
+    return "Hi all, get data from Arduino: roomId = " + op.get().getRoomId() + ", status = " + op.get().getStatus();
+  }
+}
---
+++ src/main/java/com/group8/meetingall/dto/MeetingStatusDto.java
@@ -0,0 +1,9 @@
+package com.group8.meetingall.dto;
+
+import lombok.Data;
+
+@Data
+public class MeetingStatusDto {
+  String roomId;
+  String status;
+}
