c8ad1deea9899d989293d887a8e5a868e4045ba1
AKIN GAN <akin.j.a.gan@oocl.com>
2020-12-24 20:28:02 +0800

    add the basic log4J config.

--- src/main/java/com/group8/meetingall/controller/ArduinoController.java
+++ src/main/java/com/group8/meetingall/controller/ArduinoController.java
@@ -1,6 +1,8 @@
 package com.group8.meetingall.controller;
 
-import com.group8.meetingall.dto.MeetingStatusDto;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -9,11 +11,13 @@ import java.util.Optional;
 @RequestMapping("/arduino")
 public class ArduinoController {
 
+  Logger logger = LoggerFactory.getLogger(ArduinoController.class);
+
   @GetMapping("/status")
   public String receiveMeetingStatus(@RequestParam("roomId") String roomId, @RequestParam("value") String value) {
 //    Optional<MeetingStatusDto> op = Optional.ofNullable(meetingStatusDto);
     String responseData = "Hi all, get data from Arduino: roomId = " + roomId + ", value = " + value;
-    System.out.println("[info]: " + responseData);
+    logger.info("testing : {}" + responseData);
     return responseData;
   }
 }
