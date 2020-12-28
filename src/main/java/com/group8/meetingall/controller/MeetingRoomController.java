cc15de15ac7ede1fc0940b6d12a1fcaad297cdab
AKIN GAN <akin.j.a.gan@oocl.com>
2020-12-28 20:30:06 +0800

    test API for calling ESP8266 server API.--2

--- src/main/java/com/group8/meetingall/controller/MeetingRoomController.java
+++ src/main/java/com/group8/meetingall/controller/MeetingRoomController.java
@@ -1,6 +1,8 @@
 package com.group8.meetingall.controller;
 
 import com.group8.meetingall.service.IMeetingRoomService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,9 @@ public class MeetingRoomController {
     @Autowired
     IMeetingRoomService meetingRoomService;
 
+    Logger logger = LoggerFactory.getLogger(MeetingRoomController.class);
+
+
     @GetMapping
     public ResponseEntity<Object> queryAllMeetingRooms() {
         return new ResponseEntity<Object>(meetingRoomService.queryAllMeetingRooms(), HttpStatus.OK);
@@ -23,13 +28,13 @@ public class MeetingRoomController {
 
     @GetMapping("/{id}")
     public String sendDeviceStatusToArduino(@RequestParam("deviceStatus")String deviceStatus) {
-        System.out.println("===incoming===");
+        logger.info("===incoming===");
         RestTemplate restTemplate=new RestTemplate();
         ResponseEntity<String> response = restTemplate.getForEntity("http://192.168.43.123:80/update?deviceStatus=" + deviceStatus, String.class);
 //        ResponseEntity<String> response = restTemplate.getForEntity("http://146.222.43.156:8077/api/arduino/test?roomId=" + deviceStatus, String.class);
-        System.out.println("statusCode: " + response.getStatusCode());
-        System.out.println("statusCodeValue: " + response.getStatusCodeValue());
-        System.out.println("body: " + response.getBody());
+        logger.info("statusCode: " + response.getStatusCode());
+        logger.info("statusCodeValue: " + response.getStatusCodeValue());
+        logger.info("body: " + response.getBody());
         return response.getStatusCodeValue() == 200 ? "success" : "failed";
     }
 }
