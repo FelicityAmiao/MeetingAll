d7c79ad9ccccb4c7483a0715aa63d205a060ccd6
linsa4 <samson.lin@oocl.com>
2020-12-28 16:04:02 +0800

    add file download

--- src/main/java/com/group8/meetingall/controller/FileController.java
+++ src/main/java/com/group8/meetingall/controller/FileController.java
@@ -3,8 +3,10 @@ package com.group8.meetingall.controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 import java.io.*;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
 
 @CrossOrigin
@@ -12,18 +14,29 @@ import java.nio.charset.Charset;
 @RestController
 public class FileController {
 
-    @PostMapping
-    public void getFileHighFrequencyWords(@RequestParam MultipartFile file ) {
-        String path = this.getClass().getResource("/files").getPath();
-        File filePath = new File(path);
-        if (!filePath.exists() && !filePath.isDirectory()) {
-            filePath.mkdir();
-        }
-        File targetFile = new File(path+"//"+file.getOriginalFilename());
+    @GetMapping(value = "/download/{file}")
+    public void getTranslateResultFile(HttpServletResponse response, @PathVariable(value = "file") String fileName) throws IOException {
+        OutputStream out = null;
+        String path = this.getClass().getResource("/files").getPath() + "/" + fileName;
         try {
-            file.transferTo(targetFile);
+            File file = new File(path);
+            FileInputStream fis = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            out = response.getOutputStream();
+            while ((len = fis.read(buffer)) > 0) {
+                out.write(buffer,0,len);
+            }
+            response.setContentType("application/msword;charset=UTF-8");
+            response.setHeader("Content-Disposition", "attachment;filename="
+                    .concat(URLEncoder.encode("Meeting Report - " + fileName + ".docx", "UTF-8")
+                            .replaceAll("\\+", "%20")));
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (null != out) {
+                out.close();
+            }
         }
     }
 
