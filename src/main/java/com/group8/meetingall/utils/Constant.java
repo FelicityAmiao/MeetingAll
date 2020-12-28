d8a6cba34a3e1fa885323fdcbb3d57ffa3eaf135
LIUJO5 <joi.liu@cargosmart.com>
2020-12-28 13:55:19 +0800

    add status constant

--- src/main/java/com/group8/meetingall/utils/Constant.java
+++ src/main/java/com/group8/meetingall/utils/Constant.java
@@ -1,5 +1,10 @@
 package com.group8.meetingall.utils;
 
 public class Constant {
-     public static String YYYY_MMM_DD_HH_MM_E = "yyyy-MM-dd HH:mm:ss";
+     public static final String YYYY_MMM_DD_HH_MM_E = "yyyy-MM-dd HH:mm:ss";
+     public static final String NEW = "新建";
+     public static final String RECORDER_ING = "正在录音";
+     public static final String RECORDER_FINISH = "已录音";
+     public static final String REPORT_ING = "正在生成报告";
+     public static final String REPORT_FINISHED = "已生成报告";
 }
