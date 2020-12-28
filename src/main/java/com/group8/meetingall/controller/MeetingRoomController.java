f56599418a380e15a5489510c002d47a1429f010
AKIN GAN <akin.j.a.gan@oocl.com>
2020-12-28 20:27:18 +0800

    test API for calling ESP8266 server API.

--- src/main/java/com/group8/meetingall/controller/MeetingRoomController.java
+++ src/main/java/com/group8/meetingall/controller/MeetingRoomController.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RequestMapping("/meetingRooms")
 @RestController
@@ -14,8 +16,20 @@ public class MeetingRoomController {
     @Autowired
     IMeetingRoomService meetingRoomService;
 
-    @GetMapping()
+    @GetMapping
     public ResponseEntity<Object> queryAllMeetingRooms() {
         return new ResponseEntity<Object>(meetingRoomService.queryAllMeetingRooms(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public String sendDeviceStatusToArduino(@RequestParam("deviceStatus")String deviceStatus) {
+        System.out.println("===incoming===");
+        RestTemplate restTemplate=new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity("http://192.168.43.123:80/update?deviceStatus=" + deviceStatus, String.class);
+//        ResponseEntity<String> response = restTemplate.getForEntity("http://146.222.43.156:8077/api/arduino/test?roomId=" + deviceStatus, String.class);
+        System.out.println("statusCode: " + response.getStatusCode());
+        System.out.println("statusCodeValue: " + response.getStatusCodeValue());
+        System.out.println("body: " + response.getBody());
+        return response.getStatusCodeValue() == 200 ? "success" : "failed";
     }
 }
