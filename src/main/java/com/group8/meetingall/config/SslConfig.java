7f0e996d07ddcd2074175eb1e067519665d99120
CHENFE2 <felicity.chen@cargosmart.com>
2020-12-29 20:56:16 +0800

    [Felicity] Add ssl to access backend5

--- src/main/java/com/group8/meetingall/config/SslConfig.java
+++ src/main/java/com/group8/meetingall/config/SslConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"prod1"})
+@Profile({"prod"})
 public class SslConfig {
 
   @Bean
