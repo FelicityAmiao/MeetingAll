a8ac4701b45107d2d46ee15d5ef0f57de283fc53
AKIN GAN <akin.j.a.gan@oocl.com>
2020-12-29 14:08:36 +0800

    add the API for online click start MeetingRoom devices.

--- src/main/java/com/group8/meetingall/controller/MeetingRoomController.java
+++ src/main/java/com/group8/meetingall/controller/MeetingRoomController.java
@@ -1,8 +1,10 @@
 package com.group8.meetingall.controller;
 
+import com.group8.meetingall.entity.MeetingRoom;
 import com.group8.meetingall.service.IMeetingRoomService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.group8.meetingall.service.impl.MeetingRoomServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RequestMapping("/meetingRooms")
 @RestController
@@ -20,21 +25,34 @@ public class MeetingRoomController {
 
     Logger logger = LoggerFactory.getLogger(MeetingRoomController.class);
 
+    @Autowired
+    MeetingRoomServiceImpl meetingRoomServiceImpl;
 
     @GetMapping
     public ResponseEntity<Object> queryAllMeetingRooms() {
         return new ResponseEntity<Object>(meetingRoomService.queryAllMeetingRooms(), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public String sendDeviceStatusToArduino(@RequestParam("deviceStatus")String deviceStatus) {
-        logger.info("===incoming===");
-        RestTemplate restTemplate=new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity("http://192.168.43.123:80/update?deviceStatus=" + deviceStatus, String.class);
-//        ResponseEntity<String> response = restTemplate.getForEntity("http://146.222.43.156:8077/api/arduino/test?roomId=" + deviceStatus, String.class);
-        logger.info("statusCode: " + response.getStatusCode());
-        logger.info("statusCodeValue: " + response.getStatusCodeValue());
-        logger.info("body: " + response.getBody());
-        return response.getStatusCodeValue() == 200 ? "success" : "failed";
+//    @GetMapping("/{id}")
+//    public String sendDeviceStatusToArduino(@RequestParam("deviceStatus")String deviceStatus) {
+//        logger.info("===incoming===");
+//        RestTemplate restTemplate=new RestTemplate();
+//        ResponseEntity<String> response = restTemplate.getForEntity("http://192.168.43.123:80/update?deviceStatus=" + deviceStatus, String.class);
+////        ResponseEntity<String> response = restTemplate.getForEntity("http://146.222.43.156:8077/api/arduino/test?roomId=" + deviceStatus, String.class);
+//        logger.info("statusCode: " + response.getStatusCode());
+//        logger.info("statusCodeValue: " + response.getStatusCodeValue());
+//        logger.info("body: " + response.getBody());
+//        return response.getStatusCodeValue() == 200 ? "success" : "failed";
+//    }
+
+    @PostMapping
+    public ResponseEntity updateMeetingRoomStartedStatus(@RequestBody MeetingRoom meetingRoom){
+        meetingRoomServiceImpl.updateStartedStatusByRoomId(Optional.of(meetingRoom).get());
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/{roomId}")
+    public void receiveMeetingRoomStatus(@PathVariable("roomId") String roomId, @RequestParam("status") String status) {
+        meetingRoomServiceImpl.handleMeetingRoomStatus(roomId, status);
     }
 }
--- src/main/java/com/group8/meetingall/service/IMeetingRoomService.java
+++ src/main/java/com/group8/meetingall/service/IMeetingRoomService.java
@@ -1,9 +1,14 @@
 package com.group8.meetingall.service;
 
 import com.group8.meetingall.entity.MeetingRoom;
+import com.group8.meetingall.vo.MeetingRoomVo;
 
 import java.util.List;
 
 public interface IMeetingRoomService {
     List<MeetingRoom> queryAllMeetingRooms();
+
+    void updateStartedStatusByRoomId(MeetingRoom meetingRoom);
+
+    void handleMeetingRoomStatus(String roomId, String status);
 }
