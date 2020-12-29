fc9a403f542187daebfba20706ed1a365e2bb685
CHENFE2 <felicity.chen@cargosmart.com>
2020-12-29 15:46:14 +0800

    [Felicity] Add ssl to access backend

--- src/main/java/com/group8/meetingall/MeetingallApplication.java
+++ src/main/java/com/group8/meetingall/MeetingallApplication.java
@@ -1,13 +1,34 @@
 package com.group8.meetingall;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MeetingallApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MeetingallApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
+        return tomcat;
+    }
+
+    private Connector createHTTPConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        //同时启用http（8080）、https（8077）两个端口
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(8080);
+        connector.setRedirectPort(8077);
+        return connector;
     }
 
 }
