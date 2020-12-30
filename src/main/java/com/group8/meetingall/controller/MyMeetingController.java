288610171dcdb19e6e4ab4347d166af126271e54
VincentHuang <vincent.huang@oocl.com>
2020-12-30 10:55:19 +0800

    add api for update my meeting room status

--- src/main/java/com/group8/meetingall/controller/MyMeetingController.java
+++ src/main/java/com/group8/meetingall/controller/MyMeetingController.java
@@ -5,6 +5,7 @@ import com.group8.meetingall.entity.MeetingProfile;
 import com.group8.meetingall.service.MyMeetingService;
 import com.group8.meetingall.vo.MeetingRecordVo;
 import com.group8.meetingall.vo.MeetingVo;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,6 +40,11 @@ public class MyMeetingController {
     @GetMapping("/meetingrecords/{user}")
     private List<MeetingRecordVo> getMeetingRecords(@PathVariable(value = "user") String user) {
         return meetingService.getMeetingRecords(user);
+    }
+
+    @PostMapping("/recording")
+    private ResponseEntity<Object> recording(@RequestBody MeetingProfile meeting){
+        return new ResponseEntity<Object>(meetingService.recording(meeting), HttpStatus.OK);
     }
 
     @PostMapping(value = "/upload/audio")
